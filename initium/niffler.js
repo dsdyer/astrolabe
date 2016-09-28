29e153f2f1d09c31dcd6996b67c662c2298e44f0
ddyer <ddyer@Davids-MacBook-Pro.local>
2016-09-28 18:55:00 -0500

    Niffler version number

--- initium/niffler.js
+++ initium/niffler.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Niffler
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.1.0
 // @description  Picks up gold for you!
 // @author       Dell
 // @match        *http://www.playinitium.com/*
