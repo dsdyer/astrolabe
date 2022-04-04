f587f4f6b483ef9d8a97ba1a8a06cdffd4666b88
David Dyer <dsdyer@gmail.com>
2022-04-04 15:46:54 -0500

    Revert "Set up grid components and styles"
    This reverts commit 724b5a56e6ebbd1de98c7ea7f7fb16c007a61a0e.

--- sudoku-four-years-later/src/components/Game.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Game from './Game';
-
-test('renders Game', () => {
-  render(<Game />);
-  const gameElement = screen.getByText(/Game/i);
-  expect(gameElement).toBeInTheDocument();
-});
